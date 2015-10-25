0f6da5d8c899129b2f70f60fb5c00b4ba929ea56
RahlandGordon <rahland.gordon.12@cnu.edu>
2015-10-25 15:06:47 -0400

    Update StringParserClass.cpp

--- StringParser/StringParserClass.cpp
+++ StringParser/StringParserClass.cpp
@@ -42,3 +42,79 @@ bool StringParserClass::setTags(const char *pStartMem, const char *pEndMem){
 				}
 				return areTagsSet;
 		}
+bool StringParserClass::getDataBetweenTags(char *pDataSearch, vector<string> &myVect){
+	
+	char *sTag = pDataSearch;
+	bool notEnd = true;
+	while(notEnd){
+		char *eTag = 0;
+		
+		//find start mem space
+		findTag(pStartMem, sTag, eTag);
+		
+		char *sTag2 = eTag;
+		char *eTag2 = 0;
+		
+		//find end mem space
+		findTag(pEndMem, sTag2, eTag2);
+		
+		int i = 1;
+		
+		//store data
+		std::string data = "";
+		
+		while (*(eTag + i) != *(sTag2)){
+			if(*eTag == '\0' || *sTag == '\0'){
+			break;
+		}
+		data += *(eTag + i);
+		++i;
+		}
+		if (data != ""){
+			//add data
+			myVect.push_back(data);
+		}
+		else{
+			lastError = ERROR_DATA_NULL;
+		}
+		if (*eTag2 == '\0'){
+			notEnd = false;
+		}
+		else{
+			sTag = eTag2;
+		}
+	}
+	cleanup();
+	return SUCCEEDED;
+}
+bool StringParserClass::findTag(char *pLookForSpace, char *&pS, char *&pE){
+	
+	int i = 0;
+	while(*(pS + i) != '\0'){
+		int k = i;
+		int j = 0;
+		if(*(pS + k) == *(pLookForSpace + j)){
+			while(*(pS + k) == *(pLookForSpace + j)){
+			char *t = (pStart + k);
+			++k;
+			++j;
+			if(*(pS + k) == '>'){
+				pS = pS + i;
+				pE = pS + j;
+				return true;
+			}
+		}
+	}
+	++i;
+}
+pE = pS + i;
+return false;
+}
+void StringParser::cleanup(){
+	if (pStartMem){
+		delete[] pStartMem;
+	}
+	if(pEndMem){
+		delete[] pEndMem;
+	}
+}
